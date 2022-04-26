683e93a97ed13bb779b973ca0b2b158684827931
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-04-26 12:17:10 +0600

    Deploying to heoku

---
+++ __server__deploy.js
@@ -0,0 +1,13 @@
+/**
+ * --------------------
+ * ONE TIME
+ * --------------------
+ * 1. Create Heroku Account
+ * 2. Verify email
+ * 3. Install heroku CLI
+ * 4. Heroku Login
+ * 
+ * --------------------------
+ * For each project one time
+ * --------------------------
+ */
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(403).send({ message: "Forbidden access" });
+    } else {
+      console.log("decoded", decoded);
+      req.decoded = decoded;
+      next();
+    }
+  });
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twvnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -18,30 +36,65 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
-    console.log('Genius car DB connected');
+    const orderCollection = client.db("geniusCar").collection("order");
+    console.log("Genius car DB connected");
+
+    // AUTH
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    // SERVICES API
+    // get all
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
-    app.get('/service/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const service = await serviceCollection.findOne(query);
-        res.send(service);
-    })
-    app.post('/service', async (req, res) => {
+    // get by id
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+    // post
+    app.post("/service", async (req, res) => {
       const newService = req.body;
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
-    })
-    app.delete('/service/:id', async(req, res) => {
+    });
+    // delete
+    app.delete("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
-    })
+    });
+    // ORDERS API
+    app.get("/order", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "Forbidden access" });
+      }
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
   } finally {
   }
 }
@@ -51,7 +104,7 @@ app.get("/", (req, res) => {
   res.send("Running genius server");
 });
 
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, (req, res) => {
   console.log("Listening to port", port);
